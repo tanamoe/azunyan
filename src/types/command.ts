76eed0b44784eb3e131e0b5ae03a8be66160edb6
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-05-25 05:35:16 +0700

    feat: add context menu command to type

--- src/types/command.ts
+++ src/types/command.ts
@@ -3,13 +3,18 @@ import type {
   SlashCommandBuilder,
   AutocompleteInteraction,
   Message,
+  ContextMenuCommandBuilder,
+  InteractionResponse,
 } from "discord.js";
 
 export interface PlayerCommand {
   data:
     | SlashCommandBuilder
-    | Omit<SlashCommandBuilder, "addSubcommandGroup" | "addSubcommand">;
-  execute(interaction: BaseInteraction): Promise<void | Message>;
+    | Omit<SlashCommandBuilder, "addSubcommandGroup" | "addSubcommand">
+    | ContextMenuCommandBuilder;
+  execute(
+    interaction: BaseInteraction
+  ): Promise<void | Message | InteractionResponse>;
 }
 
 export interface AutocompletePlayerCommand extends PlayerCommand {
