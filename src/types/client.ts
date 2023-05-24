f759a7f47c5c57ba3f762946c5251f639f4ac52f
Khoa Nguyen Do <khoanguyen.do@outlook.com>
2023-05-25 05:34:16 +0700

    build: remove unused type

--- src/types/client.ts
+++
@@ -1,6 +0,0 @@
-import type { AutocompletePlayerCommand, PlayerCommand } from "./command.js";
-import type { Client, Collection } from "discord.js";
-
-export interface PlayerClient extends Client {
-  commands: Collection<string, PlayerCommand | AutocompletePlayerCommand>;
-}
