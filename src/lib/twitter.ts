9ed83540050dbf09320619afc4bacab065c0dd9d
momo-p <contact@relay.akimitsu.dev>
2023-12-16 17:57:38 +0700

    fix(twitter): make `lintComplain()` function in extractor private

--- src/lib/twitter.ts
+++ src/lib/twitter.ts
@@ -12,7 +12,7 @@ export type ExtractorResponse =
 type ExtractorFn = (path: string) => Promise<ExtractorResponse>;
 
 export class Twitter {
-  lintComplain() {
+  private lintComplain() {
     return 0;
   }
 
