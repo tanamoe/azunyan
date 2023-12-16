900f3d33d1801f39e70dba0d902d2c0b04cf122f
ももぴ <41295891+momo-p@users.noreply.github.com>
2023-12-16 18:23:00 +0700

    feat(twitter): support for multiple `Tweet` extractor (#61)
    * feat(dotenv): adding dotenv support
    
    * feat(twitter): implement `fxtwitter` extractor
    
    * fix(twitter): make `twitter` command use the `Twitter` extractor
    
    * fix(twitter): make `lintComplain()` function in extractor private
    
    * ci: bump `bun.lockb`
    
    * ci: remove `dotenv` package since having native support
    
    * ci: bump `bun.lockb`

---
+++ src/lib/twitter.ts
@@ -0,0 +1,130 @@
+import { joinURL, parseURL } from "ufo";
+import {
+  VxTwitterMediaExtended,
+  VxTwitterResponse,
+} from "../types/vxtwitter.js";
+import { HTTPError } from "discord.js";
+import { FxTwitterMedia, FxTwitterResponse } from "../types/fxtwitter.js";
+
+export type ExtractorResponse =
+  | [VxTwitterResponse | null, null]
+  | [null, Error];
+type ExtractorFn = (path: string) => Promise<ExtractorResponse>;
+
+export class Twitter {
+  private lintComplain() {
+    return 0;
+  }
+
+  static normalizeUrl(path: string): [string, null] | ["", Error] {
+    const url = parseURL(path);
+    const supportedHosts: ReadonlyArray<string> = [
+      "twitter.com",
+      "x.com",
+      "vxtwitter.com",
+      "api.vxtwitter.com",
+      "fxtwitter.com",
+      "api.fxtwitter.com",
+    ];
+    if (!supportedHosts.includes(url.host ?? "")) {
+      return ["", new Error(`Unsupported url '${path}'`)];
+    }
+    return [url.pathname, null];
+  }
+
+  public static async extractTweet(path: string): Promise<ExtractorResponse> {
+    const extractors: ReadonlyArray<ExtractorFn> = [
+      Twitter.extractTweetViaFxTwitter,
+      Twitter.extractTweetViaVxTwitter,
+    ];
+    let latestError: Error | null = null;
+    for (const extractor of extractors) {
+      const [tweet, err] = await extractor(path);
+      if (err == null) {
+        return [tweet, null];
+      }
+      latestError = err;
+    }
+    return [null, latestError];
+  }
+
+  private static async extractTweetViaVxTwitter(
+    path: string,
+  ): Promise<ExtractorResponse> {
+    try {
+      const res = await fetch(joinURL("https://api.vxtwitter.com/", path));
+      const data: VxTwitterResponse = await res.json();
+      return [data, null];
+    } catch (e) {
+      return Twitter.extractError(e);
+    }
+  }
+
+  private static async extractTweetViaFxTwitter(
+    path: string,
+  ): Promise<ExtractorResponse> {
+    try {
+      const res = await fetch(joinURL("https://api.fxtwitter.com/", path));
+      const data: FxTwitterResponse = await res.json();
+      if (data.code > 500) {
+        return [null, new Error(data.message)];
+      }
+      if (data.code !== 200) {
+        return [null, null];
+      }
+      const normalized: VxTwitterResponse = {
+        date: data.tweet.created_at,
+        date_epoch: data.tweet.created_timestamp,
+        hashtags: [],
+        likes: data.tweet.likes,
+        mediaURLs: (data.tweet.media?.all || []).map(
+          (media: FxTwitterMedia) => {
+            return media.url;
+          },
+        ),
+        media_extended: (data.tweet.media?.all || []).map(
+          (media: FxTwitterMedia): VxTwitterMediaExtended => {
+            return {
+              duration_millis:
+                media.duration === undefined
+                  ? undefined
+                  : media.duration * 1000,
+              size: {
+                height: media.height,
+                width: media.width,
+              },
+              thumbnail_url: media.thumbnail_url ?? "",
+              type: media.type === "photo" ? "image" : media.type,
+              url: media.url,
+            };
+          },
+        ),
+        replies: data.tweet.replies,
+        retweets: data.tweet.retweets,
+        text: data.tweet.text,
+        tweetID: data.tweet.id,
+        tweetURL: data.tweet.url,
+        user_name: data.tweet.author.name,
+        user_screen_name: data.tweet.author.screen_name,
+        user_profile_image_url: data.tweet.author.avatar_url,
+      };
+      return [normalized, null];
+    } catch (e) {
+      return Twitter.extractError(e);
+    }
+  }
+
+  private static extractError(e: unknown): ExtractorResponse {
+    if (!(e instanceof HTTPError)) {
+      if (!(e instanceof Error)) {
+        return [null, new Error("Unknown error")];
+      }
+      return [null, e];
+    }
+    const status = e.status;
+    if (status > 500) {
+      return [null, e];
+    }
+    return [null, null];
+  }
+}
