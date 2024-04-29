a4b5b0b5ca144e150804ad13e232e948f70f13fb
Nguyen Do <khoanguyen.do@outlook.com>
2024-04-30 04:19:55 +0700

    refactor(twitter): update response type & helpers

--- src/lib/twitter.ts
+++ src/lib/twitter.ts
@@ -1,15 +1,40 @@
 import { HTTPError } from "discord.js";
 import { ofetch } from "ofetch";
 import { parseURL } from "ufo";
-import { type FxTwitterMedia, FxTwitterResponse } from "../types/fxtwitter.js";
 import type {
-  VxTwitterMediaExtended,
-  VxTwitterResponse,
-} from "../types/vxtwitter.js";
+  FxTwitterPhoto,
+  FxTwitterResponse,
+  FxTwitterTweet,
+  FxTwitterUser,
+  FxTwitterVideo,
+} from "../types/fxtwitter.js";
+import type { VxTwitterResponse } from "../types/vxtwitter.js";
 
-export type ExtractorResponse =
-  | [VxTwitterResponse | null, null]
-  | [null, Error];
+// helper
+type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
+
+export type ExtractorAuthor = Pick<
+  FxTwitterUser,
+  "screen_name" | "avatar_url" | "name"
+>;
+export type ExtractorPhoto = FxTwitterPhoto;
+export type ExtractorVideo = Omit<FxTwitterVideo, "format" | "variants">;
+export type ExtractorTweet = Omit<
+  Optional<
+    FxTwitterTweet,
+    | "is_note_tweet"
+    | "twitter_card"
+    | "lang"
+    | "source"
+    | "replying_to"
+    | "replying_to_status"
+  >,
+  "author" | "media"
+> & {
+  author: ExtractorAuthor;
+  media?: { photos?: ExtractorPhoto[]; videos?: ExtractorVideo[] };
+};
+export type ExtractorResponse = [ExtractorTweet | null, null] | [null, Error];
 type ExtractorFn = (path: string) => Promise<ExtractorResponse>;
 
 export class Twitter {
@@ -53,11 +78,14 @@ export class Twitter {
     path: string,
   ): Promise<ExtractorResponse> {
     try {
-      const data = await ofetch(path, {
+      const data = await ofetch<VxTwitterResponse>(path, {
         baseURL: "https://api.vxtwitter.com/",
         responseType: "json",
       });
-      return [data, null];
+
+      const normalize = await Twitter.normalizeVxTwitter(data);
+
+      return [normalize, null];
     } catch (e) {
       return Twitter.extractError(e);
     }
@@ -67,53 +95,20 @@ export class Twitter {
     path: string,
   ): Promise<ExtractorResponse> {
     try {
-      const data = await ofetch(path, {
+      const data = await ofetch<FxTwitterResponse>(path, {
         baseURL: "https://api.fxtwitter.com/",
         responseType: "json",
       });
+
+      // TODO: https://github.com/FixTweet/FxTwitter/wiki/Status-Fetch-API
       if (data.code > 500) {
         return [null, new Error(data.message)];
       }
       if (data.code !== 200) {
         return [null, null];
       }
-      const normalized: VxTwitterResponse = {
-        date: data.tweet.created_at,
-        date_epoch: data.tweet.created_timestamp,
-        hashtags: [],
-        likes: data.tweet.likes,
-        mediaURLs: (data.tweet.media?.all || []).map(
-          (media: FxTwitterMedia) => {
-            return media.url;
-          },
-        ),
-        media_extended: (data.tweet.media?.all || []).map(
-          (media: FxTwitterMedia): VxTwitterMediaExtended => {
-            return {
-              duration_millis:
-                media.duration === undefined
-                  ? undefined
-                  : media.duration * 1000,
-              size: {
-                height: media.height,
-                width: media.width,
-              },
-              thumbnail_url: media.thumbnail_url ?? "",
-              type: media.type === "photo" ? "image" : media.type,
-              url: media.url,
-            };
-          },
-        ),
-        replies: data.tweet.replies,
-        retweets: data.tweet.retweets,
-        text: data.tweet.text,
-        tweetID: data.tweet.id,
-        tweetURL: data.tweet.url,
-        user_name: data.tweet.author.name,
-        user_screen_name: data.tweet.author.screen_name,
-        user_profile_image_url: data.tweet.author.avatar_url,
-      };
-      return [normalized, null];
+
+      return [data.tweet, null];
     } catch (e) {
       return Twitter.extractError(e);
     }
@@ -131,5 +126,58 @@ export class Twitter {
       return [null, e];
     }
     return [null, null];
+  }
+
+  private static async normalizeVxTwitter(data: VxTwitterResponse) {
+    const photos: ExtractorPhoto[] = [];
+    const videos: ExtractorVideo[] = [];
+
+    data.media_extended.map((media) => {
+      switch (media.type) {
+        case "image":
+          photos.push({
+            url: media.url,
+            width: media.size.width,
+            height: media.size.height,
+            altText: media.altText ?? "",
+            type: "photo",
+          });
+          break;
+
+        case "gif":
+        case "video":
+          videos.push({
+            url: media.url,
+            thumbnail_url: media.thumbnail_url,
+            width: media.size.width,
+            height: media.size.height,
+            duration: media.duration_millis / 1000,
+            type: media.type,
+          });
+      }
+    });
+
+    const normalize: ExtractorTweet = {
+      id: data.tweetID,
+      url: data.tweetURL,
+      text: data.text,
+      created_at: data.date,
+      created_timestamp: data.date_epoch,
+      likes: data.likes,
+      retweets: data.retweets,
+      replies: data.replies,
+      author: {
+        name: data.user_name,
+        screen_name: data.user_screen_name,
+        avatar_url: data.user_profile_image_url,
+      },
+      possibly_sensitive: data.possibly_sensitive,
+      media: {
+        photos,
+        videos,
+      },
+    };
+
+    return normalize;
   }
 }
